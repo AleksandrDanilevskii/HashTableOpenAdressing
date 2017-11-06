3743e6ed47812218867c05c7234f5acfe7c4469d
Alexander Danilevsky <danilevskii.aleksandr97@gmail.com>
2017-11-06 19:52:19 +0300

    Main unloaded.

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.util.Scanner;
 
 public class HashTable {
     private int size;
@@ -5,9 +7,8 @@ public class HashTable {
     private int counte;
 
     private int hash(String key) {
-        System.out.println(size);
         int hash = 0;
-        for(int i = 0; i < key.length(); i++) {
+        for (int i = 0; i < key.length(); i++) {
             hash = (31 * hash + key.charAt(i)) % size;
         }
         return hash;
@@ -21,19 +22,21 @@ public class HashTable {
     public void print() {
         System.out.println("HashTable output:");
         boolean flag = true;
-        for(int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             if (table[i] != null) {
                 System.out.println(i + " " + table[i].getKey());
                 flag = false;
             }
         }
-        if(flag)
+        if (flag)
             System.out.println("HashTable is empty.");
     }
 
     public void insert(String key) {
-        if(size==0){size++;}
-        if(counte<size) {
+        if (size == 0) {
+            size++;
+        }
+        if (counte < size) {
             Item item = new Item(key);
             int hash = hash(key);
             while (table[hash] != null) {
@@ -42,30 +45,29 @@ public class HashTable {
             }
             table[hash] = item;
             counte++;
-        }
-        else{
+        } else {
             rehash();
             insert(key);
         }
     }
 
     public void rehash() {
-        this.size = 2*table.length;
+        this.size = 2 * table.length;
         Item[] newElements = new Item[this.size];
         Item[] old = table;
         table = newElements;
-        counte =0;
-        for (Item node : old){
-                insert(node.getKey());
+        counte = 0;
+        for (Item node : old) {
+            insert(node.getKey());
         }
 
     }
 
-    public void remove(String val){
+    public void remove(String val) {
         int h = hash(val);
-        for(int i=h; i<size; i++){
+        for (int i = h; i < size; i++) {
             String str = table[i].getKey();
-            if (str.equals(val)){
+            if (str.equals(val)) {
                 table[i] = null;
                 counte--;
                 break;
@@ -80,13 +82,13 @@ public class HashTable {
 
     public void isEmpty() {
         boolean flag = true;
-        for(int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             if (table[i] != null) {
                 flag = false;
                 break;
             }
         }
-        if(flag)
+        if (flag)
             System.out.println("HashTable is empty.");
         else
             System.out.println("HashTable is not empty.");
@@ -94,7 +96,7 @@ public class HashTable {
 
     public void search(String val) {
         int h = hash(val);
-        for(int i=h; i<size; i++) {
+        for (int i = h; i < size; i++) {
             String str = table[i].getKey();
             if (str.equals(val)) {
                 System.out.println("Element in.");
@@ -106,6 +108,82 @@ public class HashTable {
     }
 
     public void size() {
-        System.out.println("Size = "+size);
+        System.out.println("Size = " + size);
+    }
+
+    public static int inputSize() throws IOException {
+        int size;
+        Scanner scan = new Scanner(System.in);
+        try {
+            System.out.print("Enter size: ");
+            size = scan.nextInt();
+            if (size <= 0) throw new Exception("Вы ввели неположительное число!");
+        } catch (Exception e) {
+            System.out.println("Вы ввели недопустимые числа!");
+            return inputSize();
+        }
+        return size;
+    }
+
+    public void selectMenu() throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("HashTable Operations:");
+            System.out.println("1. Insert;");
+            System.out.println("2. Remove;");
+            System.out.println("3. Make it empty;");
+            System.out.println("4. Check empty;");
+            System.out.println("5. Search;");
+            System.out.println("6. Print;");
+            System.out.println("7. Size;");
+            System.out.println("0. Stop.");
+
+            choice = inputCase();
+
+            if (choice != 0) {
+                switch (choice) {
+                    case 1:
+                        System.out.print("Enter what insert: ");
+                        insert(scan.next());
+                        break;
+                    case 2:
+                        System.out.print("Enter what remove: ");
+                        remove(scan.next());
+                        break;
+                    case 3:
+                        makeEmpty();
+                        break;
+                    case 4:
+                        isEmpty();
+                        break;
+                    case 5:
+                        System.out.print("Enter what find: ");
+                        search(scan.next());
+                        break;
+                    case 6:
+                        print();
+                    case 7:
+                        size();
+                }
+            }
+        } while (choice != 0);
+    }
+
+
+    public int inputCase() {
+        Scanner scan = new Scanner(System.in);
+        int choice;
+        try {
+            choice = scan.nextInt();
+        } catch (NumberFormatException e) {
+            System.out.println("You have entered invalid characters! Re-enter:");
+            return inputCase();
+        } catch (Exception e) {
+            System.out.println("You entered non-positive numbers! Re-enter:");
+            return inputCase();
+        }
+        return choice;
     }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,23 @@
-import java.util.Scanner;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter size: ");
-        HashTable hashTable = new HashTable(scan.nextInt());
+        int size=0;
+        try {
+            size = HashTable.inputSize();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        int choice;
-        do {
-            System.out.println("HashTable Operations:");
-            System.out.println("1. Insert;");
-            System.out.println("2. Remove;");
-            System.out.println("3. Make it empty;");
-            System.out.println("4. Check empty;");
-            System.out.println("5. Search;");
-            System.out.println("6. Print;");
-            System.out.println("7. Size;");
-            System.out.println("0. Stop.");
+        HashTable hashTable = new HashTable(size);
 
-            choice = scan.nextInt();
-            if (choice!=0) {
-                switch (choice) {
-                    case 1:
-                        System.out.print("Enter what insert: ");
-                        hashTable.insert(scan.next());
-                        break;
-                    case 2:
-                        System.out.print("Enter what remove: ");
-                        hashTable.remove(scan.next());
-                        break;
-                    case 3:
-                        hashTable.makeEmpty();
-                        break;
-                    case 4:
-                        hashTable.isEmpty();
-                        break;
-                    case 5:
-                        System.out.print("Enter what find: ");
-                        hashTable.search(scan.next());
-                        break;
-                    case 6:
-                        hashTable.print();
-                    case 7:
-                        hashTable.size();
-                }
+        try {
+            hashTable.selectMenu();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-            }
-        }while(choice!=0);
     }
 }
