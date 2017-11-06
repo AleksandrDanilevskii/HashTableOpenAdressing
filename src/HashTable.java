199df5701c31dfd4c711422440f9752baad6f355
Alexander Danilevsky <danilevskii.aleksandr97@gmail.com>
2017-11-06 13:00:15 +0300

    Add hash function.

--- src/HashTable.java
+++ src/HashTable.java
@@ -3,6 +3,15 @@ public class HashTable {
     private int size;
     private Item[] table;
 
+    private int hash(String key) {
+        int hash = 0;
+        for(int i = 0; i < key.length(); i++) {
+            hash = (127 * hash + key.charAt(i)) % size;
+
+        }
+        return hash;
+    }
+
     public HashTable(int size) {
         this.size = size;
         table = new Item[size];
