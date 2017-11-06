752bed45b882afb4f823974fa85f9c5bd29debbb
Alexander Danilevsky <danilevskii.aleksandr97@gmail.com>
2017-11-06 13:46:35 +0300

    Improved select menu.

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.print("Enter size: ");
         HashTable hashTable = new HashTable(scan.nextInt());
 
-        char ch;
+        int choice;
         do {
             System.out.println("HashTable Operations:");
             System.out.println("1. Insert ");
@@ -17,25 +17,26 @@ public class Main {
             System.out.println("4. Size");
             System.out.println("5. Check empty");
             System.out.println("6. Contains");
+            System.out.println("0. Stop");
 
-            int choice = scan.nextInt();
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter what insert: ");
-                    hashTable.insert(scan.next());
-                    break;
-                case 2:
-                    System.out.print("Enter what remove: ");
-                    hashTable.remove(scan.next());
-                    break;
-                case 3:
-                    hashTable.makeEmpty();
-                    break;
+            choice = scan.nextInt();
+            if (choice!=0) {
+                switch (choice) {
+                    case 1:
+                        System.out.print("Enter what insert: ");
+                        hashTable.insert(scan.next());
+                        break;
+                    case 2:
+                        System.out.print("Enter what remove: ");
+                        hashTable.remove(scan.next());
+                        break;
+                    case 3:
+                        hashTable.makeEmpty();
+                        break;
+
+                }
+                hashTable.print();
             }
-            hashTable.print();
-
-            System.out.println("Type c to continue or s to stop");
-            ch = scan.next().charAt(0);
-        } while (ch == 'c');
+        }while(choice!=0);
     }
 }
