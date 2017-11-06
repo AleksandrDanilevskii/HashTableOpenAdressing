b5ce6da279d21b9e8b6837e38fb36ac07a7c8090
Alexander Danilevsky <danilevskii.aleksandr97@gmail.com>
2017-11-06 12:55:47 +0300

    Add selection menu.

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,17 @@ public class Main {
         System.out.print("Enter size: ");
         HashTable hashTable = new HashTable(scan.nextInt());
 
+        char ch;
+        do {
+            System.out.println("Hash Table Operations:");
+            System.out.println("1. insert ");
+            System.out.println("2. remove");
+            System.out.println("3. clear");
+            System.out.println("4. size");
+            System.out.println("5. check empty");
+            System.out.println("6. contains");
 
+            ch = scan.next().charAt(0);
+        } while (ch == 'c');
     }
 }
