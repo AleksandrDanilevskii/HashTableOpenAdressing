265389d603d9eb19a0f9341658ee504c40464911
Alexander Danilevsky <danilevskii.aleksandr97@gmail.com>
2017-11-06 12:51:35 +0300

    Add void new HashTable.

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,2 +1,10 @@
 public class HashTable {
+
+    private int size;
+    private Item[] table;
+
+    public HashTable(int size) {
+        this.size = size;
+        table = new Item[size];
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter size: ");
+        HashTable hashTable = new HashTable(scan.nextInt());
+
+
+    }
 }
