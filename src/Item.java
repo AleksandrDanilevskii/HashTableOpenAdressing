559b8acda04c811c98d5b6fc3a14f6da269545f1
Alexander Danilevsky <danilevskii.aleksandr97@gmail.com>
2017-11-06 12:46:03 +0300

    Add class Item.

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,16 @@
-public class Item {
+//элемент таблицы
+class Item{
+    private String key; //ключ-строка
+
+    public Item(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
 }
